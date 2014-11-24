a7214677e610861a568efdcd277195c2c3f9d5c1
Poyu Chen <poyuchen@kkbox.com>
2014-11-24 23:47:13 +0800

    Add FileProvider class and its test

---
+++ src/FileProvider.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Poyu;
+
+/**
+ * This class is a mock for testing.
+ * It reads local file and give data to client through getLine function
+ * to simulate read data from internet.
+ */
+class FileProvider implements ConnectionProvider
+{
+    private $fp = null;
+    private $root = 'tests/data';
+    private $dirPath;
+    private $fileName = 'index.html';
+
+    /**
+     * use url without special charactor as file path
+     * ex. data pf http://11.22:80 will save in directory 'http112280'
+     * and the full path will be $root/$dirPath
+     */
+    public function __construct($host, $port)
+    {
+        $removeArray = ['/', '.', ':'];
+        $this->dirPath = str_replace($removeArray, '', $host . $port);
+    }
+
+    /**
+     * file not exisit === host not exist
+     */
+    public function connect()
+    {
+        return file_exists($this->root . '/' . $this->dirPath);
+    }
+
+    public function getLine()
+    {
+        if (!$this->fp) {
+            $this->fp = fopen(
+                $this->root . '/' . $this->dirPath . '/' . $this->fileName,
+                'r'
+            );
+
+            if (!$this->fp) {
+                return false;
+            }
+        }
+
+        return fgets($this->fp);
+    }
+
+    /**
+     * To get data from client.
+     * Because we only carw about which resource client wants,
+     * this function only handle first line.
+     */
+    public function write($data)
+    {
+        $lines = explode('\n', $data);
+        $firstLine = $lines[0];
+        $splitArr = explode(' ', $firstLine);
+        //TODO? error handling
+        if ('/' !== $splitArr[1]) {
+            $this->fileName = substr($splitArr[1], 1);
+        }
+    }
+
+    /**
+     * May be not used?
+     */
+    public function getError()
+    {
+    }
+}
---
+++ tests/FileProviderTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Poyu;
+
+class FileProviderTest extends \PHPUnit_Framework_TestCase
+{
+    private $f;
+
+    public function setUp()
+    {
+        $this->f = new FileProvider('http://12.34', 56);
+    }
+
+    // Need Some Document?
+    public function testConnect()
+    {
+        $this->assertEquals(true, $this->f->connect());
+
+        $f = new FileProvider('http://aa.bb.cc', 80);
+        $this->assertEquals(false, $f->connect());
+    }
+
+
+    public function testGetLine()
+    {
+        $this->f->connect();
+        $this->assertEquals("First line\n", $this->f->getLine());
+        $this->assertEquals("Second line\n", $this->f->getLine());
+        $this->assertEquals(null, $this->f->getLine());
+    }
+
+
+    public function testWrite()
+    {
+        $reflectionClass = new \ReflectionClass('Poyu\FileProvider');
+        $property = $reflectionClass->getProperty('fileName');
+        $property->setAccessible(true);
+
+        $this->f->connect();
+        $this->f->write(
+            "GET / HTTP/1.1\r\n" .
+            "Host: http://aa.bb\r\n" .
+            "Connection: Close\r\n\r\n"
+        );
+        $this->assertEquals('index.html', $property->getValue($this->f));
+
+        $this->f->write(
+            "GET /test HTTP/1.1\r\n" .
+            "Host: http://aa.bb\r\n" .
+            "Connection: Close\r\n\r\n"
+        );
+        $this->assertEquals('test', $property->getValue($this->f));
+    }
+
+}
